e9761f95ce9186de36e92399b4700f67f25b3d65
thehimel <himeldas66@gmail.com>
2021-03-23 21:14:34 +0100

    Update solution.py

--- single-element-in-sorted-array/solution.py
+++ single-element-in-sorted-array/solution.py
@@ -5,16 +5,16 @@ def single_element(nums):
 
     while(left < right):
         mid = (left + right)//2
-        is_even = (right - mid) % 2 == 0
+        right_is_even = (right - mid) % 2 == 0
 
         if nums[mid] == nums[mid-1]:
-            if is_even:
+            if right_is_even:
                 right = mid - 2
             else:
                 left = mid + 1
 
         elif nums[mid] == nums[mid+1]:
-            if is_even:
+            if right_is_even:
                 left = mid + 2
             else:
                 right = mid - 1
